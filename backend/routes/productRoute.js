84e5ddb370188a874b65d94c7a5faa67e9b37548
Subhra sundar sinha <72126523+PavilionRYZ@users.noreply.github.com>
2024-01-16 10:28:05 +0530

    add product coontrollers,routes and api features

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,24 @@
 const express = require("express");
-const { getAllProducts, createNewProducts } = require("../controllers/productController");
+const { getAllProducts,
+     createNewProducts, 
+     getSingleProduct, 
+     updateProduct, 
+     deleteProduct, 
+     createProductReview,
+     getProductReviews,
+     deleteProductReview} = require("../controllers/productController");
+const { isAuthenticated, authRole } = require("../middleWares/auth");
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/product/new").post(createNewProducts);
+router.route("/product/new").post(isAuthenticated, authRole("admin"), createNewProducts);
+router.route("/product/:id").get(getSingleProduct);
+router.route("/product/:id")
+.put(isAuthenticated,authRole("admin"),updateProduct)
+.delete(isAuthenticated,authRole("admin"),deleteProduct);
+router.route("/review").put(isAuthenticated,createProductReview);
+router.route("/reviews")
+.get(getProductReviews)
+.delete(isAuthenticated,deleteProductReview);
 
 module.exports = router;
