6130a1ddbea6ff619f44630135f3547a5bdc58ac
Subhra sundar sinha <72126523+PavilionRYZ@users.noreply.github.com>
2024-02-21 21:46:21 +0530

    change gif folder loc

--- frontend/src/Components/Loader/Loader.jsx
+++ frontend/src/Components/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import "./Loader.scss"
-import gif from "../../assets/Images/Gifs/Bean Eater-1s-200px.gif"
+import gif from "../../assets/Gifs/Bean Eater-1s-200px.gif"
 const Loader = () => {
   return (
     <div className='loader-container'>
