f91071d0c6eee67a0dd4b5e6f687dff5b70ed5c3
Subhra sundar sinha <72126523+PavilionRYZ@users.noreply.github.com>
2024-03-05 21:49:03 +0530

    delete row folder and directly use function in home component

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -3,9 +3,6 @@ import React, { Fragment, useEffect, useState } from "react";
 import "./Home.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts } from "../../Redux/Actions/productAction";
-import WoodenRow from "../Row/WoodenRow";
-import ShareeRow from "../Row/ShareeRow";
-import ClayRow from "../Row/ClayRow";
 // import BagsRow from "../Row/BagsRow";
 import Loader from "../Loader/Loader";
 import ImageCarousel from "../Carousel/Carousel";
@@ -16,36 +13,46 @@ import image2 from "../../assets/Images/Carousel/AdobeStock_326340898_Preview.jp
 import image3 from "../../assets/Images/Carousel/AdobeStock_596562754_Preview.jpeg";
 import image4 from "../../assets/Images/Carousel/AdobeStock_655646740_Preview.jpeg";
 
-
 //import other images
-import Logo from '../../assets/Images/Home/all_logo.png'
-
-
-
+import Logo from "../../assets/Images/Home/all_logo.png";
+import ProductCard from "./ProductCard";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { products, error, loading,category } = useSelector(
+  const { products, error, loading, category } = useSelector(
     (state) => state.products
   );
-
-
-
-
 
   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]);
 
-
-
-
-//images, titles and captions for carousel
+  //images, titles and captions for carousel
   const images = [
-    { url: img, title: "Ariyas - Discover Local Treasures", caption: "Products With GI Tag's", link: "/" },
-    { url: image2, title: "Ariyas - Discover Local Treasures", caption: "Buy & Sell Local Goods", link: "/" },
-    { url: image3, title: "Ariyas - Discover Local Treasures", caption: "Empower Local Artician's", link: "/" },
-    { url: image4, title: "Ariyas - Discover Local Treasures", caption: "Start Your Journey Today", link: "/" },
+    {
+      url: img,
+      title: "Ariyas - Discover Local Treasures",
+      caption: "Products With GI Tag's",
+      link: "/",
+    },
+    {
+      url: image2,
+      title: "Ariyas - Discover Local Treasures",
+      caption: "Buy & Sell Local Goods",
+      link: "/",
+    },
+    {
+      url: image3,
+      title: "Ariyas - Discover Local Treasures",
+      caption: "Empower Local Artician's",
+      link: "/",
+    },
+    {
+      url: image4,
+      title: "Ariyas - Discover Local Treasures",
+      caption: "Start Your Journey Today",
+      link: "/",
+    },
   ];
   const [loadingButton, setLoadingButton] = useState(null);
 
@@ -54,14 +61,12 @@ const Home = () => {
       return;
     }
     setLoadingButton(buttonClass);
-    await new Promise(resolve => setTimeout(resolve, 1500));
+    await new Promise((resolve) => setTimeout(resolve, 1500));
     setLoadingButton(null);
     setTimeout(() => {
-      window.location.href = '/shop';
+      window.location.href = "/shop";
     }, 1500);
   };
-
-
 
   return (
     <Fragment>
@@ -69,54 +74,113 @@ const Home = () => {
         <Loader />
       ) : (
         <div className="home-container">
-
           <div className="carousel-container">
             <ImageCarousel images={images} />
-
           </div>
 
-          <div className="logo-container"> <img src={Logo} alt="logo" /></div>
+          <div className="logo-container">
+            {" "}
+            <img src={Logo} alt="logo" />
+          </div>
 
           <div className="category-container">
             <div className="top-container">
-
               <div className="top1">
                 <p className="top1-text">HandMade Bags</p>
-                <button className={`top1-button ${loadingButton === 'top1' ? 'loading' : ''}`}
-                  onClick={() => handleButtonClick('top1')}>Bags</button>
+                <button
+                  className={`top1-button ${
+                    loadingButton === "top1" ? "loading" : ""
+                  }`}
+                  onClick={() => handleButtonClick("top1")}
+                >
+                  Bags
+                </button>
               </div>
 
               <div className="top2">
                 <p className="top2-text">Hand-Loom Sarees</p>
-                <button className={`top2-button ${loadingButton === 'top2' ? 'loading' : ''}`}
-                  onClick={() => handleButtonClick('top2')}>Sarees</button>
+                <button
+                  className={`top2-button ${
+                    loadingButton === "top2" ? "loading" : ""
+                  }`}
+                  onClick={() => handleButtonClick("top2")}
+                >
+                  Sarees
+                </button>
               </div>
-
             </div>
 
             <div className="bottom-container">
-
               <div className="bottom1">
                 <p className="bottom1-text">Woodcrafted Items</p>
-                <button className={`bottom1-button ${loadingButton === 'bottom1' ? 'loading' : ''}`}
-                  onClick={() => handleButtonClick('bottom1')}>Woodcrafts</button>
+                <button
+                  className={`bottom1-button ${
+                    loadingButton === "bottom1" ? "loading" : ""
+                  }`}
+                  onClick={() => handleButtonClick("bottom1")}
+                >
+                  Woodcrafts
+                </button>
               </div>
 
               <div className="bottom2">
                 <p className="bottom2-text">Clay-Made Items</p>
-                <button className={`bottom2-button ${loadingButton === 'bottom2' ? 'loading' : ''}`}
-                  onClick={() => handleButtonClick('bottom2')}>Clay-Made</button>
+                <button
+                  className={`bottom2-button ${
+                    loadingButton === "bottom2" ? "loading" : ""
+                  }`}
+                  onClick={() => handleButtonClick("bottom2")}
+                >
+                  Clay-Made
+                </button>
               </div>
-
             </div>
           </div>
 
           <div className="product-row-container">
-           <WoodenRow products={products} />
-            <ClayRow products={products} /> 
-             <ShareeRow products={products} /> 
-            {/* <BagsRow products={products} />  */}
+            {/* Wooden row */}
+            <div className="row">
+              <h1>{products && products[0].category} Item&apos;s</h1>
+              <div>
+                {products &&
+                  products.slice(0, 4).map((product) => {
+                    return <ProductCard key={product._id} product={product} />;
+                  })}
+              </div>
+            </div>
 
+            {/* Clay/ceramic Row */}
+            <div className="row">
+              <h1>{products && products[4].category} Item&apos;s</h1>
+              <div>
+                {products &&
+                  products.slice(4, 8).map((product) => {
+                    return <ProductCard key={product._id} product={product} />;
+                  })}
+              </div>
+            </div>
+
+            {/* Bags Row */}
+            {/* <div className="row">
+              <h1>{products && products[13].category} Item&apos;s</h1>
+              <div>
+                {products &&
+                  products.slice(13, 16).map((product) => {
+                    return <ProductCard key={product._id} product={product} />;
+                  })}
+              </div>
+            </div> */}
+
+            {/* Sarees Row */}
+            <div className="row">
+              <h1>{products && products[8].category} Item&apos;s</h1>
+              <div>
+                {products &&
+                  products.slice(8, 12).map((product) => {
+                    return <ProductCard key={product._id} product={product} />;
+                  })}
+              </div>
+            </div>
           </div>
         </div>
       )}
