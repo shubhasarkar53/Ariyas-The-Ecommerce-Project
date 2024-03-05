05bd3c29b82163238cebcb69caceea551e198a65
Sayan Modak <sayanprodev111@gmail.com>
2024-03-05 10:46:42 +0530

    Modified the Home page

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./Home.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts } from "../../Redux/Actions/productAction";
@@ -26,11 +26,24 @@ const Home = () => {
   }, [dispatch]);
 
   const images = [
-    { url: img, title: "Ariyas - Discover Local Treasures" ,caption: "Products With GI Tag's",link:"/" },
-    { url: image2, title: "Ariyas - Discover Local Treasures" ,caption: "Buy & Sell Local Goods",link:"/" },
-    { url: image3, title: "Ariyas - Discover Local Treasures" , caption: "Empower Local Artician's",link:"/" },
-    { url: image4, title: "Ariyas - Discover Local Treasures" , caption: "Start Your Journey Today",link:"/" },
+    { url: img, title: "Ariyas - Discover Local Treasures", caption: "Products With GI Tag's", link: "/" },
+    { url: image2, title: "Ariyas - Discover Local Treasures", caption: "Buy & Sell Local Goods", link: "/" },
+    { url: image3, title: "Ariyas - Discover Local Treasures", caption: "Empower Local Artician's", link: "/" },
+    { url: image4, title: "Ariyas - Discover Local Treasures", caption: "Start Your Journey Today", link: "/" },
   ];
+  const [loadingButton, setLoadingButton] = useState(null);
+
+  const handleButtonClick = async (buttonClass) => {
+    if (loadingButton) {
+      return;
+    }
+    setLoadingButton(buttonClass);
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    setLoadingButton(null);
+    setTimeout(() => {
+      window.location.href = '/shop';
+    }, 1500);
+  };
 
   return (
     <Fragment>
@@ -41,7 +54,7 @@ const Home = () => {
 
           <div className="carousel-container">
             <ImageCarousel images={images} />
-            
+
           </div>
 
           <div className="logo-container"> <img src={Logo} alt="logo" /></div>
@@ -50,13 +63,15 @@ const Home = () => {
             <div className="top-container">
 
               <div className="top1">
-                <p className="top1-text">HandMade Bag&apos;s</p>
-                <button className="top1-button">Bags</button>
+                <p className="top1-text">HandMade Bags</p>
+                <button className={`top1-button ${loadingButton === 'top1' ? 'loading' : ''}`}
+                  onClick={() => handleButtonClick('top1')}>Bags</button>
               </div>
 
               <div className="top2">
-                <p className="top2-text">Hand-Loom Sharee&apos;s</p>
-                <button className="top2-button">Sharees</button>
+                <p className="top2-text">Hand-Loom Sarees</p>
+                <button className={`top2-button ${loadingButton === 'top2' ? 'loading' : ''}`}
+                  onClick={() => handleButtonClick('top2')}>Sarees</button>
               </div>
 
             </div>
@@ -64,13 +79,15 @@ const Home = () => {
             <div className="bottom-container">
 
               <div className="bottom1">
-                <p className="bottom1-text">Woodcrafted Item&apos;s</p>
-                <button className="bottom1-button">Woodcrafts</button>
+                <p className="bottom1-text">Woodcrafted Items</p>
+                <button className={`bottom1-button ${loadingButton === 'bottom1' ? 'loading' : ''}`}
+                  onClick={() => handleButtonClick('bottom1')}>Woodcrafts</button>
               </div>
 
               <div className="bottom2">
-                <p className="bottom2-text">Clay-Made Item&apos;s</p>
-                <button className="bottom2-button">Clay-Made</button>
+                <p className="bottom2-text">Clay-Made Items</p>
+                <button className={`bottom2-button ${loadingButton === 'bottom2' ? 'loading' : ''}`}
+                  onClick={() => handleButtonClick('bottom2')}>Clay-Made</button>
               </div>
 
             </div>
