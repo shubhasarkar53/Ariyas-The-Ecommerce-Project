1d8bb6c668e2aa2b4ce07e5fba79281596a06253
Sayan Modak <sayanprodev111@gmail.com>
2024-04-06 22:54:04 +0530

    Modified the Sale page

--- frontend/src/Components/Sale/SaleCard.jsx
+++ frontend/src/Components/Sale/SaleCard.jsx
@@ -8,6 +8,7 @@ import { FaShoppingCart } from "react-icons/fa";
 import { ToastContainer, toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
 import { addItemsToCart } from '../../Redux/Actions/cartAction.js';
+import { addItemsToWishList } from '../../Redux/Actions/wishListAction.js';
 
 const SaleCards = ({ products, isButtonClicked, onButtonClick }) => {
 
@@ -31,26 +32,25 @@ const SaleCards = ({ products, isButtonClicked, onButtonClick }) => {
     onButtonClick();
   };
 
-  const handleAddToWishlist = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+  const handleAddToWishlist = (productId, productName) => {
+    // Dispatch the addItemsToWishList action with product ID
+    dispatch(addItemsToWishList(productId));
+
+    alert(`${productName} is added to wishlist`);
+
     // Show a success toast
-    toast.success('Product added to wishlist!', {
+    toast.success(`${productName} added to wishlist!`, {
       position: toast.POSITION.TOP_RIGHT,
     });
   };
 
   const handleAddToCart = (productId, productName, productPrice) => {
-    // e.preventDefault();
-    // e.stopPropagation();
-    // // Show a success toast
-    // toast.success('Product added to cart!', {
-    //   position: toast.POSITION.TOP_RIGHT,
-    // });
     const product = products.find((product) => product._id === productId);
 
     // Dispatch the addItemsToCart action with id and quantity
     dispatch(addItemsToCart(productId, 1)); // Assuming you're adding only one item
+
+    alert(`${productName} added to cart`);
 
     // Show a success toast
     toast.success(`${productName} added to cart!`, {
@@ -108,7 +108,7 @@ const SaleCards = ({ products, isButtonClicked, onButtonClick }) => {
                           <img src={product.image[0].url} alt={product.name} />
                           {hoveredProductId === product._id && (
                             <div className="icon-container">
-                              <button onClick={(e) => handleAddToWishlist(e)}>
+                              <button onClick={() => handleAddToWishlist(product._id, product.name)}> {/* Call handleAddToWishlist with product ID and name */}
                                 <FaHeart className="wishlist-icon"
                                 />
                               </button>
@@ -144,7 +144,6 @@ const SaleCards = ({ products, isButtonClicked, onButtonClick }) => {
           </div>
         </div>
       </div>
-
     </>
   )
 }
