c456d2b1e1be2ad571555ef566acff58e9305f26
Shubha Sarkar <sarkarshubha123@gmail.com>
2024-04-07 14:38:07 +0530

    Dot loader created

---
+++ frontend/src/Components/Loader/DotLoader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DotLoader = () => {
+  return (
+    <div className="dot-animation-container">
+      <div className="animation-dot"></div>
+      <div className="animation-dot"></div>
+      <div className="animation-dot"></div>
+    </div>
+  );
+};
+
+export default DotLoader;
