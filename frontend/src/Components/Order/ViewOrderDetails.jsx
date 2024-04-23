95c9668e1fcc4fd4566ccf5f6de8d9863e77fc9c
Subhra sundar sinha <72126523+PavilionRYZ@users.noreply.github.com>
2024-04-23 12:32:45 +0530

    solve error in view order

--- frontend/src/Components/Order/ViewOrderDetails.jsx
+++ frontend/src/Components/Order/ViewOrderDetails.jsx
@@ -38,107 +38,62 @@ const ViewOrderDetails = ( { match }) => {
   <Fragment>
     {loading ? <Loader /> : (
       <Fragment>
-        <div className="orderDetailsPage">
-          <div className="orderDetailsContainer">
-            <div>
-              <p className='orderDetailsId'>
-                Order #{order && order._id}
-              </p>
-              <p className="shippingTitle">Shipping Info</p>
-              <div className="orderDetailsContainerBox">
-                <div>
-                  <p>Name:</p>
-                  <span>{order.user && order.user.name}</span>
-                </div>
-                <div>
-                  <p>Phone:</p>
-                  <span>
-                    {order.shippingInfo && order.shippingInfo.phoneNo}
-                  </span>
-                </div>
-                <div>
-                  <p>Address:</p>
-                  <span>
-                    {order.shippingInfo &&
-                      `${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.state}, ${order.shippingInfo.pinCode}, ${order.shippingInfo.country}`}
-                  </span>
-                </div>
+
+        <div className="orderDetailsContainer">
+          <div className="orderDetailsHeader">
+            <p className="orderDetailsId">
+              <b>Order ID:</b> {order && order._id}
+            </p>
+            <p className="orderDetailsId">
+              <b>Order Date:</b> {order && order.createdAt.substring(0, 10)}
+            </p>
+            <p className="orderDetailsId">
+              <b>Order Amount:</b> ${order && order.totalPrice}
+            </p>
+            <p className="orderDetailsId">
+              <b>Order Status:</b>
+               {order && order.orderStatus}
+            </p>
+          </div>
+          <div className="orderDetailsContainerBox">
+            <div className="orderDetailsContainerBoxItems">
+              <Typography>Shipping Info</Typography>
+              <div className="orderDetailsContainerBoxItem">
+                <p>
+                  <b>Name:</b> {order && order.user && order.user.name}
+                </p>
+                <p>
+                  <b>Phone:</b> {order && order.shippingInfo && order.shippingInfo.phoneNo}
+                </p>
+                <p>
+                  <b>Address:</b>{" "}
+                  {order &&
+                    order.shippingInfo &&
+                    `${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.state}, ${order.shippingInfo.country}, ${order.shippingInfo.pinCode}`}
+                </p>
               </div>
-              <Typography>Payment</Typography>
-              <div className="orderDetailsContainerBox2">
-                <div>
-                  <p
-                    className={
-                      order.paymentInfo &&
-                      order.paymentInfo.status === "succeeded"
-                        ? "greenColor"
-                        : "redColor"
-                    }
-                  >
-                    {order.paymentInfo &&
-                    order.paymentInfo.status === "succeeded"
-                      ? "PAID"
-                      : "NOT PAID"}
-                  </p>
-                </div>
-
-                <div>
-                  <p>Items Price:</p>
-                  <span>{order.itemsPrice && order.itemsPrice}</span>
-                </div>
-
-                <div>
-                  <p>Shipping Price:</p>
-                  <span>{order.shippingPrice && order.shippingPrice}</span>
-                </div>
-
-                <div>
-                  <p>Tax:</p>
-                  <span> {order.taxPrice && order.taxPrice}</span>
-                </div>
-
-                <div>
-                  <p>Total Amount:</p>
-                  <span>{order.totalPrice && order.totalPrice}</span>
-                </div>
-
+              <Typography>Payment Info</Typography>
+              <div className="orderDetailsContainerBoxItem">
+                <p>
+                  <b>Amount:</b> ${order && order.totalPrice}
+                </p>
               </div>
-
-              <Typography>Order Status</Typography>
-              <div className="orderDetailsContainerBox">
-                <div>
-                  <p
-                    className={
-                      order.orderStatus && order.orderStatus === "Delivered"
-                        ? "greenColor"
-                        : "redColor"
-                    }
-                  >
-                    {order.orderStatus && order.orderStatus}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="orderDetailsCartItems">
-              <Typography>Order Items:</Typography>
-              <div className="orderDetailsCartItemsContainer">
-                {order.orderItems &&
-                  order.orderItems.map((item) => (
-                    <div key={item.product}>
-                      <img src={item.image} alt="Product" />
-                      <Link to={`/product/${item.product}`}>
-                        {item.name}
-                      </Link>{" "}
-                      <span>
-                        {item.quantity} X ₹{item.price} ={" "}
-                        <b>₹{item.price * item.quantity}</b>
-                      </span>
-                    </div>
-                  ))}
+              <Typography>Order Items</Typography>
+              <div className="orderDetailsContainerBoxItem">
+                {order && order.orderItems && order.orderItems.map((item) => (
+                  <div key={item.product}>
+                    <img src={item.image} alt="Product" />
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    <p>{item.name}</p>
+                    <p>{item.quantity} X ${item.price} = ${item.price * item.quantity}</p>
+                  </div>
+                ))
+                }
               </div>
             </div>
           </div>
-        </div>
+          </div>
+         
         <ToastContainer/>
       </Fragment>
     )}
