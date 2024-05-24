dbc2ccbb76370c5c2cab039dbf8b468ad26c1456
Sayan Modak <sayanprodev111@gmail.com>
2024-05-24 10:37:16 +0530

    Commit changes

--- frontend/src/Components/Seller/Register-Seller/RegisterSeller.jsx
+++ frontend/src/Components/Seller/Register-Seller/RegisterSeller.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { registerSeller } from '../../../Redux/Actions/registerSellerAction';
